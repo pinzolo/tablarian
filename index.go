403b69fca999549d4dd014c8d5333fdfe39d2899
pinzolo <pinzolo@gmail.com>
2016-07-20 11:50:44 +0900

    Implements index subcommand

--- index.go
+++ index.go
@@ -1,20 +1,49 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/pinzolo/dbmodel"
+)
+
 var cmdIndex = &Command{
 	Run:       runIndex,
 	UsageLine: "index ",
-	Short:     "Enumerate table names in schema.",
+	Short:     "Enumerate all table names in schema.",
 	Long: `
 
 	`,
 }
 
 func init() {
-	// Set your flag here like below.
-	// cmdIndex.Flag.BoolVar(&flagA, "a", false, "")
+	cmdIndex.Flag.StringVar(&configFile, "config", "tablarian.config", "Config file path")
+	cmdIndex.Flag.StringVar(&configFile, "c", "tablarian.config", "Config file path")
 }
 
 // runIndex executes index command and return exit code.
 func runIndex(args []string) int {
+	cfg, err := loadConfig(configFile)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+	db := dbClientFor(cfg)
+	db.Connect()
+	defer db.Disconnect()
+
+	tables, err := db.AllTableNames(cfg.Schema)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+
+	printTableNames(tables, cfg)
 	return 0
+}
+
+func printTableNames(tables []*dbmodel.Table, cfg *Config) {
+	for _, tbl := range tables {
+		fmt.Println(tbl.Name())
+	}
 }
