85ab2432c3b0ef03ca383be3be67913b6d0e1b1f
pinzolo <pinzolo@gmail.com>
2016-07-28 21:04:13 +0900

    Add test case for long serial seq name

--- postgres_pretty_converter_test.go
+++ postgres_pretty_converter_test.go
@@ -39,6 +39,23 @@ func TestInt4ToSerial(t *testing.T) {
 	}
 }
 
+func TestInt4ToSerialLongSeqName(t *testing.T) {
+	conv := postgresPrettyConverter{}
+	size := dbmodel.NewSize(
+		sql.NullInt64{Valid: false},
+		sql.NullInt64{Int64: 32, Valid: true},
+		sql.NullInt64{Int64: 0, Valid: true},
+	)
+	col := dbmodel.NewColumn("foo", "bank_output_record_user_sales_detail", "bank_output_record_user_sales_detail_id", "Primary key of items", "int4", size, false, "nextval('bank_output_record_user_sales_bank_output_record_user_sales_seq'::regclass)", 0)
+	data := conv.ConvertColumn(&col)
+	if a, e := data[2], "serial"; a != e {
+		t.Errorf("Third value should be serial. expected: %v, actual: %v", e, a)
+	}
+	if a, e := data[5], ""; a != e {
+		t.Errorf("Sixth value should be empty on data type is serial. expected: %v, actual: %v", e, a)
+	}
+}
+
 func TestInt4ToInteger(t *testing.T) {
 	conv := postgresPrettyConverter{}
 	size := dbmodel.NewSize(
@@ -80,26 +97,11 @@ func TestInt4ToIntegerWithOtherSeq(t *testing.T) {
 	)
 	col := dbmodel.NewColumn("foo", "items", "id", "Primary key of items", "int4", size, false, "nextval('other_seq'::regclass)", 0)
 	data := conv.ConvertColumn(&col)
-	if a, e := data[0], ""; a != e {
-		t.Errorf("Primary key should be empty when PrimaryKeyPosition is 0. actual: %v", a)
-	}
-	if a, e := data[1], "id"; a != e {
-		t.Errorf("Second value should be column name. expected: %v, actual: %v", e, a)
-	}
 	if a, e := data[2], "integer"; a != e {
 		t.Errorf("Third value should be serial. expected: %v, actual: %v", e, a)
 	}
-	if a, e := data[3], ""; a != e {
-		t.Errorf("Fourth value should be empty on data type is integer. expected: %v, actual: %v", e, a)
-	}
-	if a, e := data[4], "NO"; a != e {
-		t.Errorf("Fifth value should be nullable and value should be 'NO' when column is not nullable. expected: %v, actual: %v", e, a)
-	}
 	if a, e := data[5], "nextval('other_seq'::regclass)"; a != e {
 		t.Errorf("Sixth value should be default value. expected: %v, actual: %v", e, a)
-	}
-	if a, e := data[6], "Primary key of items"; a != e {
-		t.Errorf("Seventh value should be comment. expected: %v, actual: %v", e, a)
 	}
 }
 
@@ -107,7 +109,7 @@ func TestInt2ToSmallserial(t *testing.T) {
 	conv := postgresPrettyConverter{}
 	size := dbmodel.NewSize(
 		sql.NullInt64{Valid: false},
-		sql.NullInt64{Int64: 32, Valid: true},
+		sql.NullInt64{Int64: 16, Valid: true},
 		sql.NullInt64{Int64: 0, Valid: true},
 	)
 	col := dbmodel.NewColumn("foo", "items", "id", "Primary key of items", "int2", size, false, "nextval('items_id_seq'::regclass)", 0)
@@ -135,12 +137,29 @@ func TestInt2ToSmallserial(t *testing.T) {
 	}
 }
 
+func TestInt2ToSmallserialLongSeqName(t *testing.T) {
+	conv := postgresPrettyConverter{}
+	size := dbmodel.NewSize(
+		sql.NullInt64{Valid: false},
+		sql.NullInt64{Int64: 16, Valid: true},
+		sql.NullInt64{Int64: 0, Valid: true},
+	)
+	col := dbmodel.NewColumn("foo", "bank_output_record_user_sales_detail", "bank_output_record_user_sales_detail_id", "Primary key of items", "int2", size, false, "nextval('bank_output_record_user_sales_bank_output_record_user_sales_seq'::regclass)", 0)
+	data := conv.ConvertColumn(&col)
+	if a, e := data[2], "smallserial"; a != e {
+		t.Errorf("Third value should be smallserial. expected: %v, actual: %v", e, a)
+	}
+	if a, e := data[5], ""; a != e {
+		t.Errorf("Sixth value should be empty on data type is smallserial. expected: %v, actual: %v", e, a)
+	}
+}
+
 func TestInt2ToSmallint(t *testing.T) {
 	conv := postgresPrettyConverter{}
 	size := dbmodel.NewSize(
 		sql.NullInt64{Valid: false},
-		sql.NullInt64{Int64: 10, Valid: true},
-		sql.NullInt64{Int64: 2, Valid: true},
+		sql.NullInt64{Int64: 16, Valid: true},
+		sql.NullInt64{Int64: 0, Valid: true},
 	)
 	col := dbmodel.NewColumn("foo", "items", "id", "Primary key of items", "int2", size, false, "", 0)
 	data := conv.ConvertColumn(&col)
@@ -171,31 +190,16 @@ func TestInt2ToSmallintWithOtherSeq(t *testing.T) {
 	conv := postgresPrettyConverter{}
 	size := dbmodel.NewSize(
 		sql.NullInt64{Valid: false},
-		sql.NullInt64{Int64: 10, Valid: true},
-		sql.NullInt64{Int64: 2, Valid: true},
+		sql.NullInt64{Int64: 16, Valid: true},
+		sql.NullInt64{Int64: 0, Valid: true},
 	)
 	col := dbmodel.NewColumn("foo", "items", "id", "Primary key of items", "int2", size, false, "nextval('other_seq'::regclass)", 0)
 	data := conv.ConvertColumn(&col)
-	if a, e := data[0], ""; a != e {
-		t.Errorf("Primary key should be empty when PrimaryKeyPosition is 0. actual: %v", a)
-	}
-	if a, e := data[1], "id"; a != e {
-		t.Errorf("Second value should be column name. expected: %v, actual: %v", e, a)
-	}
 	if a, e := data[2], "smallint"; a != e {
 		t.Errorf("Third value should be smallint. expected: %v, actual: %v", e, a)
 	}
-	if a, e := data[3], ""; a != e {
-		t.Errorf("Fourth value should be empty on data type is smallint. expected: %v, actual: %v", e, a)
-	}
-	if a, e := data[4], "NO"; a != e {
-		t.Errorf("Fifth value should be nullable and value should be 'NO' when column is not nullable. expected: %v, actual: %v", e, a)
-	}
 	if a, e := data[5], "nextval('other_seq'::regclass)"; a != e {
 		t.Errorf("Sixth value should be default value. expected: %v, actual: %v", e, a)
-	}
-	if a, e := data[6], "Primary key of items"; a != e {
-		t.Errorf("Seventh value should be comment. expected: %v, actual: %v", e, a)
 	}
 }
 
@@ -231,12 +235,29 @@ func TestInt8ToBigserial(t *testing.T) {
 	}
 }
 
+func TestInt8ToBigserialLongSeqName(t *testing.T) {
+	conv := postgresPrettyConverter{}
+	size := dbmodel.NewSize(
+		sql.NullInt64{Valid: false},
+		sql.NullInt64{Int64: 64, Valid: true},
+		sql.NullInt64{Int64: 0, Valid: true},
+	)
+	col := dbmodel.NewColumn("foo", "bank_output_record_user_sales_detail", "bank_output_record_user_sales_detail_id", "Primary key of items", "int8", size, false, "nextval('bank_output_record_user_sales_bank_output_record_user_sales_seq'::regclass)", 0)
+	data := conv.ConvertColumn(&col)
+	if a, e := data[2], "bigserial"; a != e {
+		t.Errorf("Third value should be bigserial. expected: %v, actual: %v", e, a)
+	}
+	if a, e := data[5], ""; a != e {
+		t.Errorf("Sixth value should be empty on data type is bigserial. expected: %v, actual: %v", e, a)
+	}
+}
+
 func TestInt8ToBigint(t *testing.T) {
 	conv := postgresPrettyConverter{}
 	size := dbmodel.NewSize(
 		sql.NullInt64{Valid: false},
-		sql.NullInt64{Int64: 10, Valid: true},
-		sql.NullInt64{Int64: 2, Valid: true},
+		sql.NullInt64{Int64: 64, Valid: true},
+		sql.NullInt64{Int64: 0, Valid: true},
 	)
 	col := dbmodel.NewColumn("foo", "items", "id", "Primary key of items", "int8", size, false, "", 0)
 	data := conv.ConvertColumn(&col)
@@ -267,31 +288,16 @@ func TestInt8ToBigintWithOtherSeq(t *testing.T) {
 	conv := postgresPrettyConverter{}
 	size := dbmodel.NewSize(
 		sql.NullInt64{Valid: false},
-		sql.NullInt64{Int64: 10, Valid: true},
-		sql.NullInt64{Int64: 2, Valid: true},
+		sql.NullInt64{Int64: 64, Valid: true},
+		sql.NullInt64{Int64: 0, Valid: true},
 	)
 	col := dbmodel.NewColumn("foo", "items", "id", "Primary key of items", "int8", size, false, "nextval('other_seq'::regclass)", 0)
 	data := conv.ConvertColumn(&col)
-	if a, e := data[0], ""; a != e {
-		t.Errorf("Primary key should be empty when PrimaryKeyPosition is 0. actual: %v", a)
-	}
-	if a, e := data[1], "id"; a != e {
-		t.Errorf("Second value should be column name. expected: %v, actual: %v", e, a)
-	}
 	if a, e := data[2], "bigint"; a != e {
 		t.Errorf("Third value should be bigint. expected: %v, actual: %v", e, a)
 	}
-	if a, e := data[3], ""; a != e {
-		t.Errorf("Fourth value should be empty on data type is bigint. expected: %v, actual: %v", e, a)
-	}
-	if a, e := data[4], "NO"; a != e {
-		t.Errorf("Fifth value should be nullable and value should be 'NO' when column is not nullable. expected: %v, actual: %v", e, a)
-	}
 	if a, e := data[5], "nextval('other_seq'::regclass)"; a != e {
 		t.Errorf("Sixth value should be default value. expected: %v, actual: %v", e, a)
-	}
-	if a, e := data[6], "Primary key of items"; a != e {
-		t.Errorf("Seventh value should be comment. expected: %v, actual: %v", e, a)
 	}
 }
 
